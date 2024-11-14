f6f9d5f5c20e7a455125522cbe475836b46795bf
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-11-14 17:28:01 -0300

    adminstracao routing e module ts

---
+++ src/app/administracao/administracao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdministracaoRoutingModule } from './administracao-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdministracaoRoutingModule
+  ]
+})
+export class AdministracaoModule { }
