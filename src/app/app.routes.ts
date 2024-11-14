f2b1f39db1534d3aa9fecda161dc10d717dff516
PietroDaniel <pietro.daniel@alunos.sc.senac.br>
2024-11-14 09:19:59 -0300

    Update app.routs.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{path: '', redirectTo: 'pruus', pathMatch: 'full'},
+
+{path: 'pruus',
+  loadChildren:() => import('./pruus/pruus.module').then(m => m.PruusModule)
+}
+
+
+
+
+
+];
